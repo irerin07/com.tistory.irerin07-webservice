cc4d413e28afb406dd563cd37d01d1847ebfbd80
irerin07 <counterchord89@gmail.com>
2020-01-05 23:52:49 +0900

    회원가입 구현을 어떻게 해야할까..

--- src/main/java/sprinboot/service/users/UserService.java
+++ src/main/java/sprinboot/service/users/UserService.java
@@ -1,4 +1,15 @@
 package sprinboot.service.users;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sprinboot.domain.user.UserRepository;
+import sprinboot.web.dto.PostsSaveRequestDto;
+
+@RequiredArgsConstructor
+@Service
 public class UserService {
+
+    private final UserRepository userRepository;
+
 }
