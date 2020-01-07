19e13fec820030f4d205762e2b0448ee28ce062d
irerin07 <counterchord89@gmail.com>
2020-01-07 23:26:16 +0900

    회원가입 만드시다

--- src/main/java/sprinboot/service/users/UserService.java
+++ src/main/java/sprinboot/service/users/UserService.java
@@ -5,11 +5,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import sprinboot.domain.user.UserRepository;
 import sprinboot.web.dto.PostsSaveRequestDto;
+import sprinboot.web.dto.UserSaveRequestDto;
 
 @RequiredArgsConstructor
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
+
+    @Transactional
+    public Long save(UserSaveRequestDto requestDto){
+        return userRepository.save(requestDto.toEntity()).getId();
+    }
 
 }
