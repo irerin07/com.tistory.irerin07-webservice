3ede907f1c0b249616969d70a5d5453524487216
irerin07 <counterchord89@gmail.com>
2020-01-10 00:05:43 +0900

    yarn.lock 수정

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>내가 만든 게시판!</h1>
     </div>
   );
 }
